415be82633d4fc4612353bee569e297ae3736ed6
Corneliu Procopenco <procorneliu@gmail.com>
2025-08-07 12:43:45 +0300

    scraping v1

--- controllers/scrapeController.ts
+++ controllers/scrapeController.ts
@@ -1,55 +1,68 @@
-import type { Request, Response, NextFunction } from 'express';
+import { type Request, type Response, type NextFunction, text } from 'express';
 import catchAsync from '../utils/catchAsync';
 import AppError from '../utils/appError';
 
 import validUrl from 'valid-url';
 import puppeteer from 'puppeteer';
 
-interface productObject {
-  [title: string]: {};
-}
-let products: {}[] = [];
+type MetaItem = {
+  property: string;
+  content: string;
+};
+
+let products: MetaItem[][] = [];
+
 const scrapeUrl = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
   const { url } = req.body;
   if (!validUrl.isUri(url)) return next(new AppError('Invalid URL', 404));
 
   const browser = await puppeteer.launch();
-  // const page = await browser.newPage();
 
-  const productsContent: productObject = {};
+  const productsContent: any = {};
 
   let queue = [url];
+  let num = 0;
 
-  while (queue.length) {
+  while (queue.length && num < 10) {
     const url = queue[queue.length - 1];
     console.log(url);
     const page = await browser.newPage();
     await page.goto(url, { timeout: 0 });
-    productsContent[url] = await page.$eval('*', (el) => {
-      const title = document.querySelector('h1, h2, h3, h4, h5, h6')?.textContent?.trim();
-      const price = document.querySelector('.price')?.textContent?.trim();
 
-      // products.push({ title });
+    productsContent[url] = await page.evaluate(() => {
+      const allowed = ['og:title', 'og:description', 'og:image', 'og:url', 'product:price:amount'];
 
-      return { title, price };
+      const metaTags = Array.from(document.querySelectorAll('meta'))
+        .map((tag) => {
+          if (allowed.includes(tag.getAttribute('property')!))
+            if (tag.getAttribute('property')! && tag.getAttribute('content')) {
+              const key = tag.getAttribute('property')!.split(':')[1].trim();
+              return {
+                [key]: tag.getAttribute('content'),
+              };
+            }
+        })
+        .filter((el) => el);
+
+      return metaTags;
     });
+
+    // Add only if this is not main page
     products.push(productsContent[url]);
     products = products.filter((product) => Object.keys(product).length > 0);
-    // productsContent[url] = await page.evaluate(() => {
-    //   const title = document.querySelector('.js-name-detail')?.textContent?.trim();
 
-    //   return { title };
-    // });
     queue.pop();
     const hrefs = await page.$$eval('a', (el) => el.map((a) => a.href));
 
     const filteredHrefs = hrefs.filter(
       (href) =>
+        href.replace('https://', '').split('/').length - 1 > 2 &&
         href.startsWith(url) &&
         !href.includes('#') &&
         !href.includes('=') &&
-        // href.includes('product/pasta-de-dinti-zettoc') &&
-        href.includes('dormitor/model') &&
+        !href.includes('contact') &&
+        !href.includes('about') &&
+        !href.includes('blog') &&
         productsContent[href] === undefined
     );
 
@@ -58,23 +71,19 @@ const scrapeUrl = catchAsync(async (req: Request, res: Response, next: NextFunct
     queue = [...new Set(queue)];
 
     await page.close();
+    num++;
+
+    // Skip saving information from main page
+    if (url === req.body.url) {
+      products.pop();
+    }
   }
-  // await page.goto(url);
-
-  // const content = await page.evaluate(() => {
-  //   const titles = document.querySelectorAll('.tm-procuct-card');
-
-  //   return Array.from(titles).map((el) => {
-  //     const title = el.querySelector('.tm-procuct-card-title')?.textContent?.trim();
-
-  //     return { title };
-  //   });
-  // });
 
   await browser.close();
 
   res.status(200).json({
     status: 'success',
+    results: products.length,
     data: products,
   });
 });
